d9ef8213eba03a21f9f50dc92dad28de16e0549c
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-06 21:01:12 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function loadFunction() {
     $.getJSON('title.json', function(data){
 		$.each(data,function(key, val) {
-			$('h2')=(data.title);
+			$('h2').innerHTML=(data.title);
 		});
 	});
     }
