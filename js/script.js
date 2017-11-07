a9cc5d7a9606e31d8057318b0ec89eaa962e3271
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-07 16:32:16 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
+var titl = {
+    title: "Заголовок",
+    urls: ["chapone.json","chaptwo.json","chapthree.json"]
+};
+var str = JSON.stringify(titl);
+arr=JSON.parse(str);
+var chapone = {text:"Первая глава"};
+str = JSON.stringify(chapone);
+chapone=JSON.parse(str);
+chaptwo = {text:"Вторая глава"};
+str = JSON.stringify(chaptwo);
+chaptwo=JSON.parse(str);
+chapthree = {text:"Третья глава"};
+str = JSON.stringify(chapthree);
+chapthree=JSON.parse(str);
+var someURL;
+var someTitle;
+
 function loadFunction() {
-    $.getJSON('title.json', function(key,value){
-		if (key=="title") {
-		$('h2').innerHTML=(value);
-		};
-	});
-    }
+    someTitle=$.getJSON('story.json');
+    title.innerHTML=someTitle.title;
+    document.getElementById("loading-title").style.display='none';
+    document.getElementById("loading-text").style.display='block';
+    for (var i=0; i<someTitle.urls.length;i++){
+        someURL=someTitle.urls[i];
+        var someVar=$.getJSON(someURL);
+        var newChap = document.createElement('p');
+        newChap.innerHTML = someVar.text;
+        text.appendChild(newChap);
+    };
+    document.getElementById("loading-text").style.display='none';
+}
