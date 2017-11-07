2c8f1a4f6382dcf24d9139edf19398d3b26963fb
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-07 17:30:25 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -6,14 +6,12 @@ function loadFunction() {
 
         for (var i=0; i<data.urls.length;i++){
             var someURL=data.urls[i];
-            var someVar;
             $.getJSON(someURL, function (chapter) {
-                someVar=chapter.text;
+                var someVar=chapter.text;
                 var newChap = document.createElement('p');
                 newChap.innerHTML = someVar;
                 text.appendChild(newChap);
             });
-            
         }
 
         document.getElementById("loading-text").style.display='none';
