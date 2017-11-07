e43686a983355523ace88e7bd7cc73ad801cac31
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-07 16:45:29 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -17,16 +17,19 @@ var someURL;
 var someTitle;
 
 function loadFunction() {
-    someTitle=$.getJSON('story.json');
-    title.innerHTML=someTitle.title;
-    document.getElementById("loading-title").style.display='none';
-    document.getElementById("loading-text").style.display='block';
-    for (var i=0; i<someTitle.urls.length;i++){
-        someURL=someTitle.urls[i];
-        var someVar=$.getJSON(someURL);
-        var newChap = document.createElement('p');
-        newChap.innerHTML = someVar.text;
-        text.appendChild(newChap);
-    };
+    $.getJSON('story.json', function (data){
+        title.innerHTML=data.title;
+        document.getElementById("loading-title").style.display='none';
+        document.getElementById("loading-text").style.display='block';
+        for (var i=0; i<data.urls.length;i++){
+            someURL=data.urls[i];
+            $.getJSON(someURL, function (text) {
+                var someVar=text.text
+            });
+            var newChap = document.createElement('p');
+            newChap.innerHTML = someVar;
+            text.appendChild(newChap);
+        }
+    });
     document.getElementById("loading-text").style.display='none';
 }
