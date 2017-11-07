cf02649b50559bde9d474e7609a97625c54d7672
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-07 18:47:06 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ function loadFunction() {
         title.innerHTML=data.title;
         document.getElementById("loading-title").style.display='none';
         document.getElementById("loading-text").style.display='block';
+        var n=0;
 
         for (var i=0; i<data.urls.length;i++){
             var someURL=data.urls[i];
             $.getJSON(someURL, function (chapter) {
                 var someVar=chapter.text;
                 var newChap = document.createElement('p');
-                var n;
-                n=(i+1);
+                n++;
                 newChap.innerHTML = ("Глава "+n+": "+someVar);
                 text.appendChild(newChap);
             });
