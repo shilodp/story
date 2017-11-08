e14d8f688628b071bcded92a3465bffc171c66eb
shilodp <30504872+shilodp@users.noreply.github.com>
2017-11-08 12:47:13 +0300

    docs

--- js/script.js
+++ js/script.js
@@ -13,9 +13,39 @@ function loadFunction() {
                 n++;
                 newChap.innerHTML = ("Глава "+n+": "+someVar);
                 text.appendChild(newChap);
+
             });
         }
 
         document.getElementById("loading-text").style.display='none';
+    });
+}
+
+
+function get(url) {
+    return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = function() {
+            if (req.status == 200) {
+                resolve(req.response);
+            }
+            else {
+                reject(Error(req.statusText));
+            }
+        };
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+        req.send();
+    });
+}
+
+function check() {
+    get('story.json').then(function(response) {
+        console.log("Success!", response);
+    }, function(error) {
+        console.error("Failed!", error);
     });
 }
